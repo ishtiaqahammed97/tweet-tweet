36dd1057c0538e93d60d6467506c347c039e8b2d
ishtiaq <isah2080@ymail.com>
2021-10-24 12:17:07 +0600

    Project done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const filterElm = document.querySelector(".search-input");
 const submitInput = document.querySelector(".submit-input");
 const submitBtn = document.querySelector(".submit-btn");
+const addOrUpdateBtn = document.querySelector("form")
 const tweetListUl = document.querySelector(".collection");
 const message = document.querySelector(".message");
 
@@ -8,7 +9,7 @@ let allTweetData = getDataFromLocalStorage();
 
 function setDataToLocalStorage(tweet) {
     let tweets = '';
-    if(localStorage.getItem("totalTweets") === null) {
+    if (localStorage.getItem("totalTweets") === null) {
         tweets = [];
         tweets.push(tweet);
         localStorage.setItem("totalTweets", JSON.stringify(tweets))
@@ -21,11 +22,11 @@ function setDataToLocalStorage(tweet) {
 
 function getDataFromLocalStorage() {
     let tweets = '';
-    if(localStorage.getItem("totalTweets") === null) {
+    if (localStorage.getItem("totalTweets") === null) {
         tweets = [];
     } else {
         tweets = JSON.parse(localStorage.getItem("totalTweets"));
-    } 
+    }
     return tweets;
 }
 
@@ -35,27 +36,31 @@ function deleteDataFromLocalStorage(id) {
         return tweet.id !== id
     })
     localStorage.setItem("totalTweets", JSON.stringify(result));
-    if(result.length === 0) location.reload();
+    if (result.length === 0) location.reload();
 }
 
 // get tweet data from allTweetData
 function showTweetData(tweetList) {
+    tweetListUl.innerHTML = '';
     let li = "";
-    if(allTweetData.length === 0) {
+    if (allTweetData.length === 0) {
         message.innerHTML = "Write your tweet"
     }
     tweetList.forEach(tweet => {
         li = document.createElement("li");
         li.className = "collection-item";
         li.id = `tweet-${tweet.id}`
-        li.innerHTML = `<strong>${tweet.tweet}</strong>  <button class="delete-tweet"> Delete</button> <span>${tweet.time} </span>`
+        li.innerHTML = `<strong>${tweet.tweet}</strong>
+        <button class="btn btn-danger delete-tweet"> Delete</button> 
+        <button class="btn btn-info edit-tweet">Edit</button> 
+          <span>${tweet.time} </span>`
 
         tweetListUl.appendChild(li)
         message.innerHTML = '';
     })
 
 }
-showTweetData(allTweetData);
+// showTweetData(allTweetData);
 
 function getTweetInput(e) {
     tweetListUl.innerHTML = '';
@@ -90,7 +95,30 @@ function getTweetInput(e) {
     }
 }
 
-function deleteTweet(e) {
+
+function findTweet(id) {
+    return allTweetData.find(tweet => tweet.id === id)
+}
+
+function populateTweetData(foundTweet) {
+    submitInput.value = foundTweet.tweet;
+    // console.log(foundTweet.tweet);
+
+    const idElm = `<input type="hidden" id="id" value=${foundTweet.id} />`
+    const editElmBtn = `<button class="btn btn-info edit-tweet ms-5 mt-1">Update</button>`
+
+    if (document.querySelector('#id')) {
+        document.querySelector('#id').setAttribute('value', foundTweet.id);
+    } else if (document.querySelector('.edit-tweet')) {
+        document.forms[0].insertAdjacentHTML('beforeend', idElm)
+        document.forms[0].insertAdjacentHTML('beforeend', editElmBtn)
+    }
+
+    submitBtn.style.display = 'none';
+}
+
+
+function deleteOrEditTweet(e) {
     /// removing from UI
     if (e.target.classList.contains('delete-tweet')) {
         const target = e.target.parentElement;
@@ -105,6 +133,15 @@ function deleteTweet(e) {
         })
         allTweetData = result;
         deleteDataFromLocalStorage(id)
+    } else if (e.target.classList.contains('edit-tweet')) {
+        const target = e.target.parentElement;
+        /// getting ID
+        const id = parseInt(target.id.split("-")[1]);
+
+        const foundTweet = findTweet(id)
+        console.log(foundTweet);
+
+        populateTweetData(foundTweet);
     }
 }
 
@@ -123,7 +160,56 @@ function filterTweets(e) {
     })
 }
 
+function editTweet(e) {
+    const tweet = submitInput.value;
+
+    const id = parseInt(e.target.previousElementSibling.value, 10)
+    // console.log(e.target.previousElementSibling);
+
+    const tweetsAfterEdited = allTweetData.map((tweets) => {
+        if (tweets.id === id) {
+            return {
+                ...tweets,
+                tweet,
+            }
+        } else { return tweets }
+    })
+    allTweetData = tweetsAfterEdited;
+    // console.log(allTweetData);
+    /// update UI
+    showTweetData(allTweetData)
+}
+
+function resetUI() {
+    // remove update button
+    // remove id
+    // show submit/tweet button
+    submitBtn.style.display = 'block';
+    document.querySelector('.edit-tweet').remove();
+    document.querySelector('#id').remove();
+}
+function addOrUpdateTweet(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("submit-btn")) {
+        getTweetInput(e)
+    } else if (e.target.classList.contains('edit-tweet')) {
+        editTweet(e)
+        // reset input
+        submitInput.value = '';
+        /// reset UI
+        resetUI()
+    }
+}
+
 /// event listeners
-submitBtn.addEventListener("click", getTweetInput)
-tweetListUl.addEventListener("click", deleteTweet)
-filterElm.addEventListener('keyup', filterTweets)
+// submitBtn.addEventListener("click", getTweetInput)
+function loadAllEventListeners() {
+    tweetListUl.addEventListener("click", deleteOrEditTweet)
+    window.addEventListener("DOMContentLoaded", showTweetData.bind(null, allTweetData))
+    addOrUpdateBtn.addEventListener("click", addOrUpdateTweet)
+
+    filterElm.addEventListener('keyup', filterTweets)
+}
+
+// call all the events
+loadAllEventListeners()
